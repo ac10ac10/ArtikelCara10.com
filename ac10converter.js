bf5ad974b8bdf3d9d79c85877df41342ecd36265
ArtikelCara10 <aoktoz1@gmail.com>
2018-06-13 12:30:04 +0800

    Update ac10converter.js

--- ac10converter.js
+++ ac10converter.js
@@ -25,4 +25,3 @@ if(a_to_vk)
 {a_to_vk.innerHTML+=a_to_va;}}
 function a_to_fa()
 {var a_to_vf=new Array();protected_links=protected_links.replace(" ","");a_to_vf=protected_links.split(",");return a_to_vf;}
-auto_safelink();
