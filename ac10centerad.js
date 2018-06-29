d44c0f4db68c06f74e9ffd90b1856c43d45f2970
ArtikelCara10 <aoktoz1@gmail.com>
2018-06-29 14:29:46 +0800

    Update ac10centerad.js

--- ac10centerad.js
+++ ac10centerad.js
@@ -1,6 +1,6 @@
-var obj0=document.getElementById(&quot;post1<data:post.id/>&quot;);
-var obj1=document.getElementById(&quot;post2<data:post.id/>&quot;);
+var obj0=document.getElementById("post1<data:post.id/>");
+var obj1=document.getElementById("post2<data:post.id/>");
 var s=obj1.innerHTML;
 var t=s.substr(0,s.length/2);
-var r=t.lastIndexOf(&quot;&lt;br&gt;&quot;);
-if(r&gt;0) {obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+4);}
+var r=t.lastIndexOf("<br>");
+if(r>0) {obj0.innerHTML=s.substr(0,r);obj1.innerHTML=s.substr(r+4);}
