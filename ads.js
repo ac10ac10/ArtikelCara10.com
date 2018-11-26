b294694eef2884f2c254b7549b801d894223de7f
ArtikelCara10 <aoktoz1@gmail.com>
2018-11-26 17:07:37 +0800

    Update ads.js

--- ads.js
+++ ads.js
@@ -34,7 +34,7 @@ videoContent.onended = contentEndedListener;
 
 // Request video ads.
 var adsRequest = new google.ima.AdsRequest();
-adsRequest.adTagUrl = 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on
+adsRequest.adTagUrl = 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on
 ';
 
 
