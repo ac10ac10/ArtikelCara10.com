aa0a596ce6137bdf76a4e5afd1bd2597d2b042b5
ArtikelCara10 <aoktoz1@gmail.com>
2018-11-27 23:54:25 +0800

    Update player.js

--- player.js
+++ player.js
@@ -2,7 +2,7 @@ var player = videojs('content_video');
 
 var options = {
   id: 'content_video',
-  adTagUrl: 'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dskippablelinear&correlator='
+  adTagUrl: 'https://ads.aerserv.com/as/?plc=1059004&cb=&ip=&ua=&url=&vpaid=&vpw=&vph=&age=&yob=&gender=&coppa=&gdpr=&gdpr_consent='
 };
 
 player.ima(options);
