2cabfb999561631b819e9da8b2267c208a4008c5
ArtikelCara10 <aoktoz1@gmail.com>
2019-01-01 23:27:51 +0800

    Update vastad.js

--- vastad.js
+++ vastad.js
@@ -1,6 +1,27 @@
-var player = videojs('content_video');
-var options = {
-  id: 'content_video',
-  adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
-};
-player.ima(options);
+var testVideo = fluidPlayer(
+"my-video",
+{
+vastOptions: {
+"adList": [
+{
+"roll": "preRoll",
+"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on"
+},
+{
+"roll": "midRoll",
+"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on",
+"timer": 8
+},
+{
+"roll": "midRoll",
+"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on",
+"timer": 10
+},
+{
+"roll": "postRoll",
+"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on"
+}
+]
+}
+}
+);
