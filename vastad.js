29bb4409150929f56466b530fdeb420fec180a68
ArtikelCara10 <aoktoz1@gmail.com>
2019-01-02 00:00:12 +0800

    Update vastad.js

--- vastad.js
+++ vastad.js
@@ -1,27 +1,11 @@
-var testVideo = fluidPlayer(
-"my-video",
-{
-vastOptions: {
-"adList": [
-{
-"roll": "preRoll",
-"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on"
-},
-{
-"roll": "midRoll",
-"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on",
-"timer": 8
-},
-{
-"roll": "midRoll",
-"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on",
-"timer": 10
-},
-{
-"roll": "postRoll",
-"vastTag": "https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on"
-}
-]
-}
-}
-);
+	var testVideo = fluidPlayer(
+        'my-video',
+        {
+            layout: 'default',
+            vastLoadedCallback: function() {console.log('vast loaded')},
+            noVastVideoCallback: function() {console.log('no vast')},
+            vastVideoSkippedCallback: function() {console.log('vast skipped')},
+            vastVideoEndedCallback: function() {console.log('vast ended')},
+            adList: [{roll: 'preRoll', vastTag: 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'}]
+        }
+    );
