96d550c82801287ee3534ef3b39b257c93c710e1
ArtikelCara10 <aoktoz1@gmail.com>
2019-01-02 00:13:23 +0800

    Update vastad.js

--- vastad.js
+++ vastad.js
@@ -6,6 +6,6 @@
             noVastVideoCallback: function() {console.log('no vast')},
             vastVideoSkippedCallback: function() {console.log('vast skipped')},
             vastVideoEndedCallback: function() {console.log('vast ended')},
-            adList: [{roll: 'preRoll', vastTag: 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'}]
+            adList: [{roll: 'preRoll', vastTag: 'http://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'}]
         }
     );
