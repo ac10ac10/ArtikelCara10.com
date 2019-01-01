229595d2aeac9900c99d4f8a504d07ec64934d0f
ArtikelCara10 <aoktoz1@gmail.com>
2019-01-01 23:15:52 +0800

    Create vastad.js

---
+++ vastad.js
@@ -0,0 +1,6 @@
+var player = videojs('content_video');
+var options = {
+  id: 'content_video',
+  adTagUrl: 'https://googleads.g.doubleclick.net/pagead/ads?ad_type=video&client=ca-video-pub-4968145218643279&videoad_start_delay=0&description_url=http%3A%2F%2Fwww.google.com&max_ad_duration=40000&adtest=on'
+};
+player.ima(options);
