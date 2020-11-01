12425758638bd789fe1bf64f681e92d337c9ca51
ArtikelCara10 <aoktoz1@gmail.com>
2020-11-01 14:47:43 +0800

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -24,7 +24,7 @@ var xxx=null;
 if(x=="1"){xxx="insurance-paradigm-secure-future.html"}
 if(x=="2"){xxx="work-from-home-on-computer.html"}
 if(x=="3"){xxx="analytics-training-career.html"}
-if(x=="4"){xxx="crypto-blockchain-explode.html"}a_to_ve[i].href="https://www.hightekno.com/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow noreferrer";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="4"){xxx="crypto-blockchain-explode.html"}a_to_ve[i].href="https://www.homedesignlessness.website/2018/08/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow noreferrer";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
